d3ba9d12080f5c59b1814ec5116c0228cb8d3613
Alexander Golden <felix@agolden.com>
2021-06-06 09:13:37 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='homeassistantclient',
+        version='0.0.1',
+        description='Client library for interacting with the Home Assistant API',
+        url='https://github.com/agolden/homeassistantclient',
+        author='alex@agolden.com',
+        license='MIT',
+        packages=['homeassistantclient'],
+        install_requires=[],
+        zip_safe=True)
